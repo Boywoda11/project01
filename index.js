f83ee62bfeef21fe6dd438333f54c3d60265af40
Boywoda11 <okthovianus11@gmail.com>
2024-05-26 01:01:23 +0700

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("tes programe");
+function Test() {
+  const number1 = 100;
+  const number2 = 200;
+  const result = number2 / number1;
+
+  return {
+    number1: number1,
+    number2: number2,
+    result: result,
+  };
+}
+
+// Hello();
+console.log(Test().result);
