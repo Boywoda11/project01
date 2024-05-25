c6829628b66eefe25659b54ab24fd5f26e9c3540
Boywoda11 <okthovianus11@gmail.com>
2024-05-26 02:02:51 +0700

    express

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
-function Test() {
-  const number1 = 100;
-  const number2 = 200;
-  const result = number2 / number1;
+const express = require("express");
+const app = express();
+const port = 3000;
 
-  return {
-    number1: number1,
-    number2: number2,
-    result: result,
-  };
-}
+// controler
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome!</h1>");
+});
 
-// Hello();
-console.log(Test().result);
+app.get("/about", (req, res) => {
+  res.send("Welcome to my about");
+});
+app.get("/blog", (req, res) => {
+  res.send("Welcome to blog");
+});
+app.get("/galeri", (req, res) => {
+  res.send(
+    "<img src='https://th.bing.com/th/id/R.636e921d804ef3bb586fca10e5258c89?rik=KZkLHaDogU8e4g&riu=http%3a%2f%2fjustcoolidea.ru%2fwp-content%2fuploads%2f2015%2f11%2fcofe.jpg&ehk=fid%2bSk4ogr%2bYdJogOQS5SKCTOItPkXNl1rDk%2b37992A%3d&risl=&pid=ImgRaw&r=0'>"
+  );
+});
+
+// ini untuk server
+app.listen(port, () => {
+  console.log("sistem sudah berjalan");
+});
